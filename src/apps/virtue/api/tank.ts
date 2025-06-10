383ba06c320954b19e895ca8e019f70e43447576
Jarek Lin <s410673074@gmail.com>
2025-06-10 17:32:46 +0800

    feat: update interface

--- src/apps/virtue/api/tank.ts
+++ src/apps/virtue/api/tank.ts
@@ -1,11 +1,11 @@
 import { Transaction } from '@iota/iota-sdk/transactions';
 import { WalletAccount } from '@iota/wallet-standard';
-import { buildDepositStabilityPoolTx } from '@virtue/sdk';
+import { buildDepositStabilityPoolTx, buildWithdrawStabilityPoolTx } from '@virtue/sdk';
 
 import { IotaNetworks } from '@/types';
 
 import { getVirtueClient } from './config';
-import { DepositStabilityPoolIntentionData } from '../types/api';
+import { DepositStabilityPoolIntentionData, WithdrawStabilityPoolIntentionData } from '../types/api';
 
 export const getDepositStabilityPoolTx = async (
   txbParams: DepositStabilityPoolIntentionData,
@@ -17,6 +17,20 @@ export const getDepositStabilityPoolTx = async (
   const tx = new Transaction();
   const virtueClient = getVirtueClient(network, account);
   await buildDepositStabilityPoolTx(virtueClient, tx as any, account.address, vusdAmount, recipient);
+
+  return tx;
+};
+
+export const getWithdrawStabilityPoolTx = async (
+  txbParams: WithdrawStabilityPoolIntentionData,
+  account: WalletAccount,
+  network: IotaNetworks,
+): Promise<Transaction> => {
+  const { vusdAmount, recipient } = txbParams;
+
+  const tx = new Transaction();
+  const virtueClient = getVirtueClient(network, account);
+  await buildWithdrawStabilityPoolTx(virtueClient, tx as any, account.address, vusdAmount, recipient);
 
   return tx;
 };
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { CoreHelper } from '@/apps/msafe-core/helper';
 import { PlainTransactionHelper } from '@/apps/plain-transaction/helper';
 import { MSafeApps } from '@/apps/registry';
 
-export const appHelpers = new MSafeApps([new CoreHelper(), new PlainTransactionHelper()]);
+import { VirtueHelper } from './apps/virtue/helper';
+
+export const appHelpers = new MSafeApps([new CoreHelper(), new PlainTransactionHelper(), new VirtueHelper()]);
